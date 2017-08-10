033a2f95d2ab6f47738dde1eba082cb1520ed281
Tanmore <tanmore@mail.ru>
2017-08-10 13:29:56 +0300

    Группировка продаж каждого продукта за период

---
+++ test3.sql
@@ -0,0 +1,20 @@
+﻿#DECLARE FromDate Datetime default "2017-01-01";
+#DECLARE ToDate Datetime default '2017-01-09';
+
+SET @FromDate:='2017-01-02';
+SET @ToDate:='2017-01-03';
+SELECT DATE(SaleData),
+       Product,  
+       SUM(Amount) 
+FROM Sales 
+WHERE DATE(SaleData) BETWEEN @FromDate AND @ToDate
+GROUP BY Product, 
+         DATE(SaleData) 
+ORDER BY SaleData, 
+         Product;
+
+
+
+#SELECT Product, SUM(Amount) FROM Sales  WHERE SaleData BETWEEN @FromDate AND @ToDate;
+    
+#b-tree    
