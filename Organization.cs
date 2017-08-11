a039897b4cf6d9cb5c0cdf7b6ec8db066b09b3ae
Tanmore <tanmore@mail.ru>
2017-08-11 09:45:28 +0300

    Class Organization

---
+++ Organization.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test6
+{
+    public class Organization
+    {
+        public string Id;
+        public string EnglishName; 
+        public string ChineseName; 
+        public ContactPerson ContactPerson;
+        public Phone Phone;
+        public Fax Fax;
+    }
+}
