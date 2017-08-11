ea4c1ccfe3e35682d9fca1d30cd4b278701d5f0a
Tanmore <tanmore@mail.ru>
2017-08-11 09:50:44 +0300

    Class ContactPerson

---
+++ ContactPerson.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test6
+{
+    public class ContactPerson
+    {
+        public string EnglishFirstName;
+        public string EnglishLastName;
+        public string ChineseFirstName; 
+        public string ChineseLastName;
+        public MobilePhone MobilePhone;
+        public Phone Phone;
+        public Fax Fax;
+    }
+}
