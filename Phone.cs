28e791a3cb7f67597d54ce0b9ce3523676958c2a
Tanmore <tanmore@mail.ru>
2017-08-11 09:51:21 +0300

    Class Phone

---
+++ Phone.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test6
+{
+    public class Phone
+    {
+        public string CountryCode;
+        public string AreaCode;
+        public string Number;
+    }
+}
